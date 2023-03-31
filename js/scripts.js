c38b8f95281f04e046e17921d54e1bf08cc10e52
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-31 15:53:04 -0700

    Testing branching logic on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,21 @@
 function numberReturn(event) {
   event.preventDefault();
   const userInput = document.getElementById("userInput").value;
-  
-  
+  const words =["Beep!","Boop!","Won't you be my neighbor?"];
+  let input = [];
+  if (input === "1") {
+    output = "Beep!";
+  } 
+  if (input === "2") {
+    output = "Boop!";
+  } else {
+    output = "Try Again!"
+  }
+
+
+
   document.getElementById("robotspeak").reset();
-  document.getElementById("output").innerText = userInput;
+  document.getElementById("output").innerText = output;
   document.querySelector("p#output").removeAttribute("class");
 }
 
